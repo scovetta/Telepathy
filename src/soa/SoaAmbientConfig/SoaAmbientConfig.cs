30483f457e24712409f7adf92b4288439e1b5259
Zihao Chen <chen.zihao@microsoft.com>
2018-09-17 14:43:39 +0800

    Add HpcTrace binary ref and SoaAmbientConfig

---
+++ src/soa/SoaAmbientConfig/SoaAmbientConfig.cs
@@ -0,0 +1,37 @@
+﻿namespace SoaAmbientConfig
+{
+    using System;
+
+    public class SoaAmbientConfig
+    {
+        private static bool? standAlone;
+
+        public static bool StandAlone
+        {
+            set
+            {
+                if (!standAlone.HasValue)
+                {
+                    standAlone = value;
+                }
+                else if (standAlone.Value != value)
+                {
+                    throw new Exception($"StandAlone has been set to {standAlone}.");
+                }
+            }
+            get
+            {
+                if (!standAlone.HasValue)
+                {
+                    throw new Exception("Value do not be set.");
+                }
+                else
+                {
+                    return standAlone.Value;
+                }
+            }
+        }
+
+        public static string StorageCredential { get; set; }
+    }
+}
