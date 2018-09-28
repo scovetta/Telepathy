f665b6322839620b5169bfe2e40ca1f430385273
Zihao Chen <chen.zihao@microsoft.com>
2018-09-28 05:55:05 +0000

    Add excel.exe.config for diag, and (un)register.bat

---
+++ src/xl/ExcelRunner/HPCExcelService/register.bat
@@ -0,0 +1,3 @@
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe Microsoft.Hpc.Scheduler.dll /regfile:scheduler.reg
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe Microsoft.Hpc.excel.dll /regfile:excel.reg
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe Microsoft.Hpc.excel.dll /tlb:excel.tlb
---
+++ src/xl/ExcelRunner/HPCExcelService/unregister.bat
@@ -0,0 +1,3 @@
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe Microsoft.Hpc.Scheduler.dll /u
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe Microsoft.Hpc.excel.dll /u
+C:\Windows\Microsoft.NET\Framework\v4.0.30319\RegAsm.exe Microsoft.Hpc.excel.dll /tlb:excel.tlb /u
