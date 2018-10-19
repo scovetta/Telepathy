0c56c14b266d45729291a2040e9911adccd9b8e3
Zihao Chen <chen.zihao@microsoft.com>
2018-10-19 15:43:44 +0800

    SoaContext

---
+++ src/HpcCommon/HpcContext/SoaContext/SoaContext.cs
@@ -0,0 +1,16 @@
+﻿namespace Microsoft.Hpc.SoaContext
+{
+    using System.Threading;
+
+    // TODO: Remove me
+    public class SoaContext : IHpcContext
+    {
+        public static IHpcContext Instance = new SoaContext();
+
+        public CancellationToken CancellationToken { get; }
+
+        public IFabricContext FabricContext => SoaFabricContext.Instance;
+
+        public IRegistry Registry { get; }
+    }
+}
---
+++ src/HpcCommon/HpcContext/SoaContext/SoaFabricContext.cs
@@ -0,0 +1,40 @@
+﻿namespace Microsoft.Hpc.SoaContext
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    using System.Threading;
+    using System.Threading.Tasks;
+
+    // TODO: remove me
+    public class SoaFabricContext : IFabricContext
+    {
+        public static IFabricContext Instance = new SoaFabricContext();
+
+        private static readonly string LocalHost = "localhost";
+
+        private static readonly string[] LocalHostArr = new[] { LocalHost };
+
+        public async Task<string> ResolveSingletonServicePrimaryAsync(string serviceName, CancellationToken token)
+        {
+            return LocalHost;
+        }
+
+        public async Task<IEnumerable<string>> ResolveStatelessServiceNodesAsync(string serviceName, CancellationToken token)
+        {
+            return LocalHostArr;
+        }
+
+        public async Task<IEnumerable<string>> GetNodesAsync(CancellationToken token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public IRegistry Registry => throw new NotImplementedException();
+
+        public EndpointsConnectionString ConnectionString => throw new NotImplementedException();
+
+        public HpcContextOwner Owner => throw new NotImplementedException();
+    }
+}
