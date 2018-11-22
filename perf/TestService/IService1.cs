db2da7dd7fa7a995af71b4cd5e1cc2786f2451ec
Zihao Chen <chen.zihao@microsoft.com>
2018-11-22 12:37:25 +0800

    Add TestService

---
+++ perf/TestService/IService1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.ServiceModel;
+using Microsoft.Hpc.Scheduler.Session;
+
+namespace TestService
+{
+    // NOTE: If you change the interface name "IService1" here, you must also update the reference to "IService1" in App.config.
+    [ServiceContract]
+    public interface IService1
+    {
+        [OperationContract(Name = "ComputeWithInputData")]
+        [FaultContract(typeof(ArgumentException))]
+        [ServiceKnownType(typeof(ArgumentException))]
+        [FaultContract(typeof(RetryOperationError), Action = RetryOperationError.Action)]
+        ReqData GetData(int millisec, byte[] input_data, string commonData_dataClientId, long responseSize, DateTime sendStart);
+    }
+
+    public class ReqData
+    {
+        public string CCP_TASKINSTANCEID = string.Empty;
+
+        public byte[] responseData = null;
+
+        public long commonDataSize = -1;
+        public DateTime commonDataAccessStartTime;
+        public DateTime commonDataAccessStopTime;
+        public DateTime requestStartTime;
+        public DateTime requestEndTime;
+        public DateTime sendStart;
+    }
+}
