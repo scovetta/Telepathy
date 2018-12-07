472e07b22d0469022fbe39a8312a9bb920b4537d
Zihao Chen <chen.zihao@microsoft.com>
2018-12-07 16:00:08 +0800

    Add create-package script, config default log directory to be C:\logs, config servicePointManager for echoclient

--- create-apppackage.ps1
+++ create-apppackage.ps1
@@ -4,3 +4,4 @@ Copy-Item "$env:REPO_ROOT\src\soa\BrokerOutput\Debug" -Destination "$env:REPO_RO
 Copy-Item "$env:REPO_ROOT\src\soa\CcpServiceHost\bin\Debug" -Destination "$env:REPO_ROOT\artifacts\AzureBatchAppPackage\CcpServiceHost" -Recurse
 Copy-Item "$env:REPO_ROOT\src\soa\EchoSvcLib\bin\Debug" -Destination "$env:REPO_ROOT\artifacts\AzureBatchAppPackage\EchoSvcLib" -Recurse
 Copy-Item "$env:REPO_ROOT\samples\batch\EchoSvcSample\Registration" -Destination "$env:REPO_ROOT\artifacts\AzureBatchAppPackage\Registration" -Recurse
+Compress-Archive -Path "$env:REPO_ROOT\artifacts\AzureBatchAppPackage\*" -DestinationPath "$env:REPO_ROOT\artifacts\AzureBatchAppPackage.zip" -Force
