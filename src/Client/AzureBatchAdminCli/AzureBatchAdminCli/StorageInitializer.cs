a7644298086c93c9b00b2cfe83a25aaf7652e172
Zihao Chen <chen.zihao@microsoft.com>
2019-03-18 15:36:21 +0800

    Add StorageInitializer

---
+++ src/Client/AzureBatchAdminCli/AzureBatchAdminCli/StorageInitializer.cs
@@ -0,0 +1,30 @@
+﻿namespace AzureBatchAdminCli
+{
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using Microsoft.WindowsAzure.Storage;
+    using Microsoft.WindowsAzure.Storage.Blob;
+
+    using Serilog;
+
+    internal class StorageInitializer
+    {
+        private CloudStorageAccount account;
+
+        private static string[] RequiredBlobContainerNames { get; } = new[] { "runtime", "service-assembly", "service-registration" };
+
+        public StorageInitializer(string connectionString)
+        {
+            this.account = CloudStorageAccount.Parse(connectionString);
+        }
+
+        public async Task CreateContainersAsync()
+        {
+            var containerClient = account.CreateCloudBlobClient();
+            var tasks = RequiredBlobContainerNames.Select(containerClient.GetContainerReference).Select(c => c.CreateIfNotExistsAsync());
+            await Task.WhenAll(tasks);
+            Log.Information($"Done creating containers.");
+        }
+    }
+}
