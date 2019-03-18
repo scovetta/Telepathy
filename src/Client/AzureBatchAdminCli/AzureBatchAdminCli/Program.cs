ac0f66e8aeec33ed97aae38bcaf34e29fbdb6f2a
Zihao Chen <chen.zihao@microsoft.com>
2019-03-18 15:25:38 +0800

    Added AzureBatchAdminCli. Implemented StorageCleaner.

---
+++ src/Client/AzureBatchAdminCli/AzureBatchAdminCli/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace AzureBatchAdminCli
+{
+    using System.Threading.Tasks;
+
+    using Serilog;
+
+    class Program
+    {
+        private const string ConnectionString = "";
+
+        static async Task Main(string[] args)
+        {
+            
+            var logger = new LoggerConfiguration().WriteTo.Console().CreateLogger();
+            Log.Logger = logger;
+
+            Console.WriteLine("Hello World!");
+
+            StorageCleaner cleaner = new StorageCleaner(ConnectionString);
+            await cleaner.CleanAsync();
+        }
+    }
+}
