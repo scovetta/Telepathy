fcfa2b25ead1d644332684bd8b6c5b0ef3542a9f
Zihao Chen <chen.zihao@microsoft.com>
2019-08-14 14:50:26 +0800

    Add rebuild, remove unnecessary reference

--- set_alias.bat
+++ set_alias.bat
@@ -5,4 +5,5 @@ doskey src=cd %~dp0src
 doskey test=cd %~dp0test
 doskey soa=cd %~dp0src\soa
 doskey build=cd %~dp0 $T build.bat
+doskey rebuild=cd %~dp0 $T rebuild.bat
 doskey unittest=cd %~dp0 $T unittest.bat
