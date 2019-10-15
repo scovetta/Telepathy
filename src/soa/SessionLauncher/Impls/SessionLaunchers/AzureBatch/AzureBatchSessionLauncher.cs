19dc5bfca1232304c6ee58b2ff990d8965c1ac8d
Jialiang Hu <Jialiang.Hu@microsoft.com>
2019-10-15 15:21:12 +0800

    Move assembly to shared folder and delete it after completed.

--- src/soa/SessionLauncher/Impls/SessionLaunchers/AzureBatch/AzureBatchSessionLauncher.cs
+++ src/soa/SessionLauncher/Impls/SessionLaunchers/AzureBatch/AzureBatchSessionLauncher.cs
@@ -44,6 +44,10 @@ namespace Microsoft.Telepathy.Internal.SessionLauncher.Impls.SessionLaunchers.Az
 
         private const string ServiceRegistrationContainer = "service-registration";
 
+        private const string AzureBatchSharedWorkingDir = @"%AZ_BATCH_NODE_SHARED_DIR%\source";
+
+        private const string AzureBatchNodeSharedDir = "%AZ_BATCH_NODE_SHARED_DIR%";
+
         private const string AzureBatchTaskWorkingDir = "%AZ_BATCH_TASK_WORKING_DIR%";
 
         private const string AzureBatchPrepJobWorkingDir = "%AZ_BATCH_JOB_PREP_WORKING_DIR%";
@@ -54,6 +58,8 @@ namespace Microsoft.Telepathy.Internal.SessionLauncher.Impls.SessionLaunchers.Az
 
         private const string JobPrepCmdLine =
             @"cmd /c ""sc.exe config NetTcpPortSharing start= demand & reg ADD ^""HKLM\Software\Microsoft\StrongName\Verification\*,*^"" /f & reg ADD ^""HKLM\Software\Wow6432Node\Microsoft\StrongName\Verification\*,*^"" /f""";
+
+        private static string JobReleaseCmdLine = $@"cmd /c rd /s /q {AzureBatchSharedWorkingDir}";
 
         private static TimeSpan SchedulingTimeout = TimeSpan.FromMinutes(5);
 
@@ -476,7 +482,7 @@ namespace Microsoft.Telepathy.Internal.SessionLauncher.Impls.SessionLaunchers.Az
 
                         //Establish a link via ev between TELEPATHY_SERVICE_WORKING_DIR and AZ_BATCH_JOB_PREP_WORKING_DIR
                         env.Add(new EnvironmentSetting(ServiceRegistrationWorkingDirEnvVar, AzureBatchPrepJobWorkingDir));
-                        env.Add(new EnvironmentSetting(ServiceWorkingDirEnvVar, AzureBatchTaskWorkingDir));
+                        env.Add(new EnvironmentSetting(ServiceWorkingDirEnvVar, AzureBatchSharedWorkingDir));
                         return env;
                     }
                     var environment = ConstructEnvironmentVariable();
@@ -491,7 +497,7 @@ namespace Microsoft.Telepathy.Internal.SessionLauncher.Impls.SessionLaunchers.Az
                         }
                         else
                         {
-                            rf = ResourceFile.FromStorageContainerUrl(sasToken, blobPrefix: blobPrefix);
+                            rf = ResourceFile.FromStorageContainerUrl(sasToken, filePath:"source", blobPrefix: blobPrefix);
                         }
 
                         return rf;
@@ -510,6 +516,9 @@ namespace Microsoft.Telepathy.Internal.SessionLauncher.Impls.SessionLaunchers.Az
                         job.JobPreparationTask = new JobPreparationTask(JobPrepCmdLine);
                         job.JobPreparationTask.UserIdentity = new UserIdentity(new AutoUserSpecification(elevationLevel: ElevationLevel.Admin, scope: AutoUserScope.Task));
                         job.JobPreparationTask.ResourceFiles = new List<ResourceFile>() { GetResourceFileReference(ServiceRegistrationContainer, null) };
+
+                        job.JobReleaseTask = new JobReleaseTask(JobReleaseCmdLine);
+                        job.JobReleaseTask.UserIdentity = new UserIdentity(new AutoUserSpecification(elevationLevel: ElevationLevel.Admin, scope: AutoUserScope.Task));
 
                         // List<ResourceFile> resourceFiles = new List<ResourceFile>();
                         // resourceFiles.Add(GetResourceFileReference(RuntimeContainer, BrokerFolder));
@@ -588,7 +597,7 @@ namespace Microsoft.Telepathy.Internal.SessionLauncher.Impls.SessionLaunchers.Az
                             resourceFiles.Add(GetResourceFileReference(RuntimeContainer, CcpServiceHostFolder));
                             resourceFiles.Add(GetResourceFileReference(ServiceAssemblyContainer, startInfo.ServiceName.ToLower()));
 
-                            CloudTask cloudTask = new CloudTask(taskId, $@"cmd /c %{ServiceWorkingDirEnvVar}%\ccpservicehost\CcpServiceHost.exe -standalone");
+                            CloudTask cloudTask = new CloudTask(taskId, $@"cmd /c move {AzureBatchTaskWorkingDir}\source {AzureBatchNodeSharedDir} & %{ServiceWorkingDirEnvVar}%\ccpservicehost\CcpServiceHost.exe -standalone");
                             cloudTask.ResourceFiles = resourceFiles;
                             cloudTask.UserIdentity = new UserIdentity(new AutoUserSpecification(elevationLevel: ElevationLevel.Admin, scope: AutoUserScope.Pool));
                             cloudTask.EnvironmentSettings = cloudTask.EnvironmentSettings == null ? environment : environment.Union(cloudTask.EnvironmentSettings, comparer).ToList();
