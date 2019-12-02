e334345bf4a1cc355412a553088b20dd40d0df87
Jingjing Li <lijingjing1008@gmail.com>
2019-12-02 07:17:46 +0000

    Add cmd help text for logging.

--- src/soa/SessionAPI.Internal/ILogConfigOption.cs
+++ src/soa/SessionAPI.Internal/ILogConfigOption.cs
@@ -9,49 +9,49 @@ namespace Microsoft.Telepathy.Session.Internal
         [Option('l', HelpText = "Set log configuration only")]
         bool ConfigureLogging { get; set; }
 
-        [Option("Logging")]
+        [Option("Logging", HelpText = "Accept 'Enable' or 'Disable' to enable or disable logging feature")]
         string Logging { get; set; }
 
-        [Option("ConsoleLogging")]
+        [Option("ConsoleLogging", HelpText = "Set true to enable Console logging or false to disable Console logging")]
         bool? ConsoleLogging { get; set; }
 
-        [Option("ConsoleLoggingLevel")]
+        [Option("ConsoleLoggingLevel", HelpText = "The minimum logging level for console, accept 'Verbose'/'Debug'/'Information'/'Warning'/'Error'/'Fatal'")]
         string ConsoleLoggingLevel { get; set; }
 
-        [Option("SeqLogging")]
+        [Option("SeqLogging", HelpText = "Set true to enable Seq logging or false to disable Seq logging") ]
         bool? SeqLogging { get; set; }
 
-        [Option("SeqLoggingLevel")]
+        [Option("SeqLoggingLevel", HelpText = "The minimum logging level for Seq, accept 'Verbose'/'Debug'/'Information'/'Warning'/'Error'/'Fatal'")]
         string SeqLoggingLevel { get; set; }
 
-        [Option("SeqServerUrl")]
+        [Option("SeqServerUrl", HelpText = "The Seq Server Url")]
         string SeqServerUrl { get; set; }
 
-        [Option("AzureAnalyticsLogging")]
+        [Option("AzureAnalyticsLogging", HelpText = "Set true to enable AzureAnalytics logging or false to disable AzureAnalytics logging")]
         bool? AzureAnalyticsLogging { get; set; }
 
-        [Option("AzureAnalyticsLoggingLevel")]
+        [Option("AzureAnalyticsLoggingLevel", HelpText = "The minimum logging level for AzureAnalytics, accept 'Verbose'/'Debug'/'Information'/'Warning'/'Error'/'Fatal'")]
         string AzureAnalyticsLoggingLevel { get; set; }
 
-        [Option("AzureAnalyticsWorkspaceId")]
+        [Option("AzureAnalyticsWorkspaceId", HelpText = "The WorkSpaceId of AzureAnalytics")]
         string AzureAnalyticsWorkspaceId { get; set; }
 
-        [Option("AzureAnalyticsAuthenticationId")]
+        [Option("AzureAnalyticsAuthenticationId", HelpText = "The AuthenticationId of AzureAnalytics")]
         string AzureAnalyticsAuthenticationId { get; set; }
 
-        [Option("LocalFileLogging")]
+        [Option("LocalFileLogging", HelpText = "Set true to enable File logging or false to disable File logging")]
         bool? LocalFileLogging { get; set; }
 
-        [Option("LocalFileLoggingLevel")]
+        [Option("LocalFileLoggingLevel", HelpText = "The minimum logging level for File, accept 'Verbose'/'Debug'/'Information'/'Warning'/'Error'/'Fatal'")]
         string LocalFileLoggingLevel { get; set; }
 
-        [Option("LocalFilePath")]
+        [Option("LocalFilePath", HelpText = "The file path for writing log")]
         string LocalFilePath { get; set; }
 
-        [Option("LocalFileFormatter")]
+        [Option("LocalFileFormatter", HelpText = "The content format for File logging")]
         string LocalFileFormatter { get; set; }
 
-        [Option("RollingInterval")]
+        [Option("RollingInterval", HelpText = "The rolling interval for File logging")]
         string RollingInterval { get; set; }
     }
 }
